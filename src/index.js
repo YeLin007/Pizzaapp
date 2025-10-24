03dc4ad01b2f7025c65c192b1d6b2883f8275911
Your Name <youremail@yourdomain.com>
2025-10-24 13:02:07 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div>
+      <h1>Andy's Pizza Co.</h1>
+      <img
+        src="\pizzas\spinaci.jpg"
+        alt="Spinach Pizza"
+        width="300"
+      />
+      <h2>Spinach Pizza</h2>
+      <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
+      <p>$10</p>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
